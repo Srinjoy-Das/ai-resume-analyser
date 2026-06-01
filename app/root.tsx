f3dc21479810ea01f0b7e800822052c2dd7a193c
Srinjoy <srinjoyd373@gmail.com>
2026-06-01 23:53:40 +0530

    logo change

--- app/root.tsx
+++ app/root.tsx
@@ -13,12 +13,16 @@ import { usePuterStore } from "./lib/puter";
 import { useEffect } from "react";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
+
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
     crossOrigin: "anonymous",
   },
+
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
