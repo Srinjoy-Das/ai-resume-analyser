7b41d3a23449d909416fd8bf887d27a64ed6d261
Srinjoy <srinjoyd373@gmail.com>
2026-06-01 20:51:57 +0530

    Resume Feedback Components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => ([
@@ -12,7 +15,7 @@ const Resume = () => {
     const {id} = useParams();
     const [imageUrl, setImageUrl] = useState('');
     const [resumeUrl, setResumeUrl] = useState('');
-    const [feedback, setFeedback] = useState('');
+    const [feedback, setFeedback] = useState<Feedback | null>(null);
 
     const navigate = useNavigate();
 
@@ -71,11 +74,13 @@ const Resume = () => {
                 </section>
 
                 <section className="feedback-section">
-                    <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+                    <h2 className="text-2xl !text-black font-bold">Resume Review</h2>
 
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                            Summary ATS Details
+                            <Summary feedback={feedback}/>
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                            <Details feedback={feedback}/>
                         </div>
 
                     ) : (
